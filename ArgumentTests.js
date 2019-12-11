2d31f2bddcf256c59cbfecadb14e70d4e44ed28f
Fran Mackinlay <FranMackinlay@Mac-Pro-AFTER.local>
2019-12-11 20:32:53 -0300

    IntroJs Dia 2

--- ArgumentTests.js
+++ ArgumentTests.js
@@ -1,13 +1,13 @@
 if (process.argv[0] == '-bp') {
-  console.log('Warning: Overriding Process!')
+    console.log('Warning: Overriding Process!')
 } else {
-  if (process.platform == 'darwin') {
-    console.log('Estas usando un SO: MacOS');
-  } else if (process.platform == 'linux') {
-    console.log('Estas usando un SO: Linux');
-  } else if (process.platform == 'win32') {
-    console.log('Estas usando un SO: Windows');
-  } else console.log('No se que SO estas usando');
+    if (process.platform == 'darwin') {
+        console.log('Estas usando un SO: MacOS');
+    } else if (process.platform == 'linux') {
+        console.log('Estas usando un SO: Linux');
+    } else if (process.platform == 'win32') {
+        console.log('Estas usando un SO: Windows');
+    } else console.log('No se que SO estas usando');
 }
 
 
@@ -15,3 +15,10 @@ if (process.argv[0] == '-bp') {
 console.log('------------------');
 // var argv = process.argv.push(1, 2, 3, 4, 5);
 // console.log(process.argv);
+
+let array = [1, 2, 3];
+
+for (const value of array) {
+    console.log(value);
+    array.push(4);
+}
---
+++ Ejercicios.js
@@ -0,0 +1,32 @@
+// Ejercicio 1
+console.log('Ejercicio 1:');
+const hola = 'h o l a';
+const pruebaDeCosas = 'pru e bad ecosas';
+
+const joinFunction = value => value.replace(/\s/g, '');
+
+console.log(joinFunction(hola));
+console.log(joinFunction(pruebaDeCosas));
+
+console.log('--------------------------');
+// Ejercicio 2
+console.log('Ejercicio 2:');
+
+const arrayFunction = (array) => {
+    let total = 0;
+    for (const value of array) {
+        if (typeof value === 'number') {
+            total += value;
+        } else {
+            total -= value;
+        }
+    }
+    return total;
+}
+console.log(arrayFunction([1, 2, 3, '4']));
+
+console.log('--------------------------');
+// Ejercicio 3
+console.log('Ejercicio 3:');
+
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
